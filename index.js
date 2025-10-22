b3da4971dca8aad2718822f7c80112ae28fd684c
Moeenc137 <moeenc137@gmail.com>
2025-10-22 08:35:14 +0500

    javascript pra

---
+++ index.js
@@ -0,0 +1,22 @@
+// operations
+
+// arthmatic operators:
+
+let a = 7;
+let b = 8;
+
+console.log("a + b = ", a + b);
+console.log("a * b = ", a * b);
+console.log("a - b = ", a - b);
+console.log("a / b = ", a / b);
+console.log("a % b = ", a % b);
+console.log("a ** b = ", a ** b);
+
+// urany operators:
+
+++a; //pre-increment
+--b; //pre-decrement
+a++; //post-decrement
+b++; //post-decrement
+
+console.log("a =", a, "b =", b);

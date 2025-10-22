2039c55cb24c280ceff0c9a7662b7867e8d91838
Moeenc137 <moeenc137@gmail.com>
2025-10-22 18:18:48 +0500

    javascript prctc(operators)

--- index.js
+++ index.js
@@ -3,20 +3,42 @@
 // arthmatic operators:
 
 let a = 7;
-let b = 8;
+let b = "8";
 
-console.log("a + b = ", a + b);
-console.log("a * b = ", a * b);
-console.log("a - b = ", a - b);
-console.log("a / b = ", a / b);
-console.log("a % b = ", a % b);
-console.log("a ** b = ", a ** b);
+// console.log("a + b = ", a + b);
+// console.log("a * b = ", a * b);
+// console.log("a - b = ", a - b);
+// console.log("a / b = ", a / b);
+// console.log("a % b = ", a % b);
+// console.log("a ** b = ", a ** b);
 
 // urany operators:
 
-++a; //pre-increment
---b; //pre-decrement
-a++; //post-decrement
-b++; //post-decrement
+// ++a; //pre-increment
+// --b; //pre-decrement
+// a++; //post-decrement
+// b++; //post-decrement
 
-console.log("a =", a, "b =", b);
+// console.log("a =", a, "b =", b);
+
+//assignment operators:
+
+// a += 4; //a = a + 4
+// // a -= 4; //a = a - 4
+// // a /= 4; //a = a / 4
+// // a *= 4; //a = a * 4
+// console.log("a =", a);
+
+// comparison operators:
+
+// a == b; //is a = b? only see value
+// // a != b; //is a not = b? only see value
+// // a === b; // // // also see data type
+// // a !== b; // // // also see data type
+
+// a < b; is a greater then b?
+// a > b; is a less then b?
+// a <=b; is a greater then or equal to b?
+// a >=b; is a less then or equal to b?
+
+console.log("a != b", a != b);
